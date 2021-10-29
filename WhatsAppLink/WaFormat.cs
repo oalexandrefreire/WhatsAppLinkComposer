cb5f01035609a0e6f23807653b2da48464f8f004
Alexandre Freire <alexandre.ferreira@setic.ro.gov.br>
2021-10-29 10:06:53 -0400

    add format text options

---
+++ WhatsAppLink/WaFormat.cs
@@ -0,0 +1,32 @@
+﻿namespace WhatsAppLink
+{
+    //https://faq.whatsapp.com/general/chats/how-to-format-your-messages/
+    public static class WaFormat
+    {
+        public static string Italic(string text)
+        {
+            return $" _{RemoveSpace(text)}_ ";
+        }
+
+        public static string Bold(string text)
+        {
+            return $" *{RemoveSpace(text)}* ";
+        }
+        
+        public static string Strikethrough(string text)
+        {
+            return $" ~{RemoveSpace(text)}~ ";
+        }    
+        
+        public static string Monospace(string text)
+        {
+            return $" ```{RemoveSpace(text)}``` ";
+        }
+
+        private static string RemoveSpace(string text)
+        {
+            return text.Trim();
+        }
+        
+    }
+}
---
+++ WhatsAppLinkTest/WaFormatTest.cs
@@ -0,0 +1,36 @@
+﻿using WhatsAppLink;
+using Xunit;
+
+namespace WhatsAppLinkTest
+{
+    public class WaFormatTest
+    {
+        [Fact]
+        public void FormatBold()
+        {
+            var result = WaFormat.Bold("text");
+            Assert.Equal(" *text* ",result);
+        }
+        
+        [Fact]
+        public void FormatItalic()
+        {
+            var result = WaFormat.Italic("text");
+            Assert.Equal(" _text_ ",result);
+        }
+        
+        [Fact]
+        public void FormatMonospace()
+        {
+            var result = WaFormat.Monospace("text");
+            Assert.Equal(" ```text``` ",result);
+        }
+   
+        [Fact]
+        public void FormatStrikethrough()
+        {
+            var result = WaFormat.Strikethrough("text");
+            Assert.Equal(" ~text~ ",result);
+        }
+    }
+}
